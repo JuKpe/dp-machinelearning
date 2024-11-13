b16abb03312b185c0753956229e03e25d2f31ce8
JuKpe <149210632+JuKpe@users.noreply.github.com>
2024-11-13 13:06:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,13 @@ st.info('This is app builds a machine learning model!')
 st.write('Hello Juan Carlos!')
 
 with st.expander('DATA'):
-  st.write('Raw Data')
+  st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/JuKpe/data/refs/heads/master/penguins_cleaned.csv')
   df
+
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  X
+
+  st.write('**y**')
+  y = df.species
